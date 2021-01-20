312b0345dcfecc60dac3462b30f647509cf58d4e
ynf <y-n-f@foxmail.com>
2021-01-20 16:39:41 +0800

    排序

--- week1/src/Task2.java
+++ week1/src/Task2.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class Task2 {
     public static void main(String[] args) {
-        int[] nums = new int[] {1,2,2,3};
+        int[] nums = new int[] {6,2,2,3};
+        Arrays.sort(nums);
+
         for (int num : nums) {
             System.out.println(num);
         }
