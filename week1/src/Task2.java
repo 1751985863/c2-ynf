cb95c91f84f0fe33f38dd1d1de6a56d37b8158ea
ynf <y-n-f@foxmail.com>
2021-01-20 16:39:41 +0800

    第一天的作业朴实无华

---
+++ week1/src/Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,2,3};
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
