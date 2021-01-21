68564e0f62c47124e55874478ca90bda6bc4e461
ynf <y-n-f@foxmail.com>
2021-01-21 23:17:52 +0800

    这是第二题

--- week1/src/Task2.java
+++ week1/src/Task2.java
@@ -1,12 +1,26 @@
 import java.util.Arrays;
 
 public class Task2 {
-    public static void main(String[] args) {
-        int[] nums = new int[] {6,2,2,3};
-        Arrays.sort(nums);
 
-        for (int num : nums) {
-            System.out.println(num);
+    public static void main(String[] args) {
+        Integer a = 1,b=2;
+        System.out.println(Arrays.toString(exchange(a,b)));
+        int [] n = new int[] {1,2,3,4};
+        change(n);
+        System.out.println(Arrays.toString(n));
+    }
+    public static int[] exchange(Integer a,Integer b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        return new int[]{a,b};
+    }
+    public static void change(int[] n) {
+
+        for (int l = 0,r = n.length - 1; l < r; l++,r--) {
+            int temp = n[l];
+            n[l] = n[r];
+            n[r] = temp;
         }
     }
 }
