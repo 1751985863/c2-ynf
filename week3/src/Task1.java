29913f82cf391c42173d8c7600c74b26d324ad8b
ynf <y-n-f@foxmail.com>
2021-01-29 12:19:19 +0800

    第一题1

--- week3/src/Task1.java
+++ week3/src/Task1.java
@@ -1,5 +1,18 @@
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        //判断 a b 大小 ，a > b 1 反之 0
+        int n = fab(2, 1);
+        System.out.println(n);
+
     }
+    public static int fab(int a,int b) {
+        if (a > b) {
+            return 1;
+        }
+        return 0;
+    }
+
+
+
 }
